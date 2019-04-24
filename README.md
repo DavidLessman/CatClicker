80fa2d4db4901dfc73a2489ee7bced1d7086674a
David J Lessman <35240546+TiberiusDarkTigger@users.noreply.github.com>
2019-04-24 13:53:26 -0500

    can select different cats, they appear on the screen. clicking updates their specific score. issues with total score currently. Adding menu next.

--- README.md
+++ README.md
@@ -5,16 +5,22 @@ optional project during Udacity Frontend Dev Course.
 
 after loading the index.html file, you simply click on the cat and your score will increase.
 
-#### Part 2 Cat Clicker Duo
+#### Part 3 Cat Clicker Premium
 ~~so now I need to have 2 cats, each with a name and number of clicks.~~
 
-Two cats displaying now. Each has a name and it's own counter. Total score at the top.
+~~Two cats displaying now. Each has a name and it's own counter. Total score at the top.~~
+
+Currently working on menu for cats. Many cats (7 currently) stored in an array. they can be brought up individually. clicks stored seperately. 
 
 ## plans
 
 ~~currently pretty simple.~~
 ~~less simple now. about to see where part 3 takes me.~~
 Starting on part 3. I need a list of cats (by name), he said dropdown list verbally but it just says a list in the specs.
+
+about to create menu section. Currently able to select cats, and scores stored individually.
+
+Total score not currently functioning.
 
 going to work through the improvements suggested during this assignment. 
 
--- js/app.js
+++ js/app.js
@@ -42,17 +42,41 @@ let catList = [
     },
 ];
 
-function generateCat(i) {
+function displayCat(i) {
+    Board.innerHTML = '';
     var para = document.createElement("div");
     para.classList.add("cat");
-    para.innerHTML = `<h3>Name: <span id="name">${catList[i].name}</span>  Clicks: <span id ="clicks${i}">${catList[i].score}</span > <h3><img class="catPic${i}" src="${catList[i].img}">`;
-    document.getElementById("PlayField").appendChild(para);
+    para.innerHTML = `<h3>Name: <span id="name">${catList[i].name}</span>  Clicks: <span id ="clicks">${catList[i].score}</span > <h3><img class="catPic" id="catPic" src="${catList[i].img}">`;
+    Board.appendChild(para);
+    var clickCounter = document.getElementById('clicks');
+    var pictureOfThisCat = document.getElementById('catPic');
+
+    
+
+    pictureOfThisCat.addEventListener('click', (function(evt) {
+        return function () {
+            console.log(`You clicked on ${catList[i].name}`);
+            catList[i].score += 1;
+
+            for (var s = 0; s < catList.length; s++) {
+                siteScore += catList[s].score;
+            }
+
+            //siteScore = catList[0].score + catList[1].score;
+
+            scoreDisplay.innerText = siteScore;
+            clickCounter.innerText = catList[i].score;
+            
+        };
+    })(i) );
+    
 }
 
-for (var i = 0; i < catList.length; i++) {
-    generateCat(i);
-}
+//for (var i = 0; i < catList.length; i++) {
+//    displayCat(i);
+//}
 
+displayCat(4);
 
 function resetGame() {
     siteScore = 0;
@@ -61,23 +85,23 @@ function resetGame() {
     };
 }
 
-function respondToClick(evt) {
-    if (evt.target.classList == `catPic0`) {
-        console.log(`You clicked on ${catList[0].name}`);
-        catList[0].score += 1;
-    } else if (evt.target.classList == "catPic1") {
-        console.log(`You clicked on ${catList[1].name}`);
-        catList[1].score += 1;
-    } else {
-        console.log("Please click on the cat!")
-    };
+//function respondToClick(evt) {
+//    if (evt.target.classList == `catPic`) {
+//        console.log(`You clicked on ${catList[0].name}`);
+//        catList[0].score += 1;
+//    } else if (evt.target.classList == "catPic1") {
+//        console.log(`You clicked on ${catList[1].name}`);
+//        catList[1].score += 1;
+//    } else {
+//        console.log("Please click on the cat!")
+//    };
 
-    siteScore = catList[0].score + catList[1].score;
-    scoreDisplay.innerText = siteScore;
-    document.getElementById(`clicks${0}`).innerText = catList[0].score;
-    document.getElementById(`clicks${1}`).innerText = catList[1].score;
-}
+//    siteScore = catList[0].score + catList[1].score;
+//    scoreDisplay.innerText = siteScore;
+//    document.getElementById(`clicks${0}`).innerText = catList[0].score;
+//    document.getElementById(`clicks${1}`).innerText = catList[1].score;
+//}
 
 resetGame();
 
-Board.addEventListener('click', respondToClick);
+//Board.addEventListener('click', respondToClick);
